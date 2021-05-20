5a0bfcdafa0eef22fb41ade6f432f64616458c5f
Alex <mendelberg44@gmail.com>
2021-05-20 14:50:53 +0300

    l13 hw2 vw

--- lesson13/hw2/calculator.js
+++ lesson13/hw2/calculator.js
@@ -1,9 +1,16 @@
 /* eslint-disable no-nested-ternary */
-export const calc = function (expression) {
+export const calc = expression => {
   if (typeof expression !== 'string') return null;
-  const [a, operator, b] = expression.split(' ').map(char => (isFinite(char) ? +char : char));
+  const [a, operator, b] = expression
+    .split(' ').map(char => (isFinite(char) ? +char : char));
 
   return `${expression} = ${
-    operator === '+' ? a + b : operator === '-' ? a - b : operator === '*' ? a * b : a / b
+    operator === '+'
+    ? a + b
+    : operator === '-'
+    ? a - b
+    : operator === '*'
+    ? a * b
+    : a / b
   }`;
 };
