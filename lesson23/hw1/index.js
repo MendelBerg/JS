bffa4be43b88a918a81ab7693c5be4005b8c4174
Alex <mendelberg44@gmail.com>
2021-05-25 16:41:42 +0300

    l23 hw1 v1

--- lesson23/hw1/index.js
+++ lesson23/hw1/index.js
@@ -1,3 +1,7 @@
+/* eslint-disable no-shadow */
+/* eslint-disable no-use-before-define */
+/* eslint-disable no-restricted-syntax */
+
 const listElem = document.querySelector('.list');
 
 const tasks = [
@@ -8,7 +12,7 @@ const tasks = [
   { text: 'Buy meat', done: true },
 ];
 
-const renderTasks = tasksList => {
+function renderTasks(tasksList) {
   const tasksElems = tasksList
     .sort((a, b) => a.done - b.done)
     .map(({ text, done }) => {
@@ -19,6 +23,7 @@ const renderTasks = tasksList => {
       checkbox.setAttribute('type', 'checkbox');
       checkbox.checked = done;
       checkbox.classList.add('list__item-checkbox');
+      checkbox.addEventListener('click', event => toggleTask(event.target));
 
       if (done) {
         listItemElem.classList.add('list__item_done');
@@ -30,14 +35,40 @@ const renderTasks = tasksList => {
     });
 
   listElem.append(...tasksElems);
-};
+}
 
+function toggleTask(checkbox) {
+  checkbox.parentElement.classList.toggle('list__item_done');
+
+  for (const el of tasks) {
+    if (el.text === checkbox.parentElement.textContent) {
+      el.done = !el.done;
+      break;
+    }
+  }
+
+  refreashList();
+}
+
+function createTask() {
+  const inputElem = document.querySelector('.task-input');
+  if (!inputElem.value) {
+    return null;
+  }
+
+  tasks.push({ text: inputElem.value, done: false });
+  inputElem.value = '';
+  refreashList();
+  return undefined;
+}
+
+function refreashList() {
+  const listElem = document.querySelector('.list');
+  listElem.innerHTML = '';
+
+  renderTasks(tasks);
+}
+
+const btnCreate = document.querySelector('.create-task-btn');
+btnCreate.addEventListener('click', createTask);
 renderTasks(tasks);
-
-const checkboxArray = [...document.querySelectorAll('.list__item-checkbox')];
-
-checkboxArray.forEach(checkbox => {
-  checkbox.addEventListener('click', console.log('oki'));
-});
-
-// const toggleTask = value;
