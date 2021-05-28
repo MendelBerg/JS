d0e7e4f6f7bd8e14e2399462254b3cfbe181eea3
Alex <mendelberg44@gmail.com>
2021-05-28 18:30:10 +0300

    task3

---
+++ lesson27/task3/src/scripts/index.js
@@ -0,0 +1,15 @@
+import { renderTask } from './renderer.js';
+import { initTodoListHandlers } from './todoList.js';
+
+document.addEventListener('DOMContentLoaded', () => {
+  renderTask();
+  initTodoListHandlers();
+});
+
+const onStorageChange = e => {
+  if (e.key === 'taskList') {
+    renderTask();
+  }
+};
+
+window.addEventListener('storage', onStorageChange);
