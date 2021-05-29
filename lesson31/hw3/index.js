aaac3dfff99478b3a0938dcddeeb52a2ef28d153
Alex <mendelberg44@gmail.com>
2021-05-29 17:11:45 +0300

    lesson 31 all

---
+++ lesson31/hw3/index.js
@@ -0,0 +1,10 @@
+const delay = period => {
+  const p = new Promise(resolve => {
+    setTimeout(() => resolve(), period);
+  });
+
+  return p;
+};
+
+
+delay(3000).then(() => console.log('Done'));
