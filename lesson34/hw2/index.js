8e522b1f61ff62ea22a1f778fe7eb3ca4888a807
Alex <mendelberg44@gmail.com>
2021-05-31 07:05:13 +0300

    l34 hw2

--- lesson34/hw2/index.js
+++ lesson34/hw2/index.js
@@ -1,4 +1,11 @@
+/* eslint-disable no-use-before-define */
 const baseUrl = 'https://60b39c004ecdc1001747f926.mockapi.io/api/t1/user';
+
+const errorMessageElem = document.querySelector('.error-text');
+const allInputs = [...document.querySelectorAll('.form-input')];
+const submitBtn = document.querySelector('.submit-button');
+
+isValid(allInputs);
 
 function createUser(userData) {
   return fetch(baseUrl, {
@@ -16,41 +23,37 @@ function clearInputs(arrInputs) {
   });
 }
 
-const getInputs = () => [
-  document.querySelector('input[type="email"]'),
-  document.querySelector('input[name="name"]'),
-  document.querySelector('input[type="password"]'),
-];
-
-
-
-const submitBtn = document.querySelector('.submit-button');
 submitBtn.addEventListener('click', event => {
   event.preventDefault();
 
-  // createUser({
-  //   email: inputs[0].value,
-  //   name: inputs[1].value,
-  //   password: inputs[2].value,
-  // }).then(res => {
-  //   console.log(res);
-  //   alert(res);
-  //   clearInputs(inputs);
-  // });
+  createUser({
+    email: allInputs[0].value,
+    name: allInputs[1].value,
+    password: allInputs[2].value,
+  }).then(res => {
+    console.log(res);
+    alert(res);
+    clearInputs(allInputs);
+    isValid(allInputs);
+  });
 });
 
-
-const isValid = arr => {
-  let correct = true;
-  arr.forEach(input => {
-    if (!input.reportValidity()) {
-      correct = false;
-    }
-  });
-
-  if (correct) {
+function isValid(arr) {
+  if (arr[0].reportValidity() && arr[1].reportValidity() && arr[2].reportValidity()) {
     submitBtn.removeAttribute('disabled');
+    errorMessageElem.textContent = '';
+  } else if (!submitBtn.hasAttribute('disabled')) {
+    submitBtn.setAttribute('disabled', true);
+    errorMessageElem.textContent = 'Failed to create user';
+  } else {
+    errorMessageElem.textContent = 'Failed to create user';
   }
-};
+}
 
-isValid(getInputs());
+allInputs.forEach(e => {
+  console.log(e);
+  e.onblur = () => isValid(allInputs);
+  e.onfocus = () => {
+    errorMessageElem.textContent = '';
+  };
+});
