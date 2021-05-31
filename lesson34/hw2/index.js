bee783a9bda7b8f94eda5998f2e129f3039b6344
Alex <mendelberg44@gmail.com>
2021-05-31 15:02:55 +0300

    hw2 l34 v10

--- lesson34/hw2/index.js
+++ lesson34/hw2/index.js
@@ -1,14 +1,18 @@
 /* eslint-disable no-param-reassign */
-/* eslint-disable no-use-before-define */
 
-// ======================SOURCE==================
-
-const errorMessageElem = document.querySelector('.error-text');
-const allInputs = [...document.querySelectorAll('.form-input')];
-const submitBtn = document.querySelector('.submit-button');
-const form = document.querySelector('.login-form');
-
+// =======================SOURCE===============
 const baseUrl = 'https://60b39c004ecdc1001747f926.mockapi.io/api/t1/user';
+
+export const errorMessageElem = document.querySelector('.error-text');
+export const allInputs = [...document.querySelectorAll('.form-input')];
+export const submitBtn = document.querySelector('.submit-button');
+export const form = document.querySelector('.login-form');
+
+function clearInputs(arrInputs) {
+  arrInputs.forEach(inputElem => {
+    inputElem.value = '';
+  });
+}
 
 function createUser(userData) {
   return fetch(baseUrl, {
@@ -20,38 +24,23 @@ function createUser(userData) {
   }).then(response => response.json());
 }
 
-function clearInputs(arrInputs) {
-  arrInputs.forEach(inputElem => {
-    inputElem.value = '';
-  });
-}
-
 function isValid() {
   if (form.reportValidity()) {
     return true;
   }
 
-  enabledBtn = false;
   submitBtn.setAttribute('disabled', true);
   errorMessageElem.textContent = 'Failed to create user';
 
   return false;
 }
 
-// =================VALIDATION=================
-let enabledBtn = true;
-
-allInputs.forEach(e => {
-  e.onblur = () => {
-    enabledBtn = true;
-    errorMessageElem.textContent = '';
-  };
-});
+// =============VALIDATION===========
 
 submitBtn.addEventListener('click', event => {
-  if (!isValid()) return;
-
   event.preventDefault();
+
+  if (!isValid()) return;
 
   createUser({
     email: allInputs[0].value,
@@ -63,13 +52,11 @@ submitBtn.addEventListener('click', event => {
   });
 });
 
-form.addEventListener('click', _ => {
-  if (
-    allInputs[0].value !== '' &&
-    allInputs[1].value !== '' &&
-    allInputs[2].value !== '' &&
-    enabledBtn
-  ) {
-    submitBtn.removeAttribute('disabled');
-  }
+allInputs.forEach(inputEl => {
+  inputEl.addEventListener('blur', _ => {
+    if (submitBtn.hasAttribute('disabled')) {
+      submitBtn.removeAttribute('disabled');
+      errorMessageElem.textContent = '';
+    }
+  });
 });
