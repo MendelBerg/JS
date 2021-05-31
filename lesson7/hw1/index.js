137a8f446ed12c72206fdd8881f329e468a94597
Alex <mendelberg44@gmail.com>
2021-05-31 09:25:35 +0300

    refactor hw1,3 l7

--- lesson7/hw1/index.js
+++ lesson7/hw1/index.js
@@ -1,1 +1,1 @@
-const squareArray = numArr => (Array.isArray(numArr) ? numArr.map(number => number ** 2) : null);
+const squareArray = numArr => (!Array.isArray(numArr) ? null : numArr.map(number => number ** 2));
--- lesson7/hw3/index.js
+++ lesson7/hw3/index.js
@@ -1,2 +1,5 @@
-const increaseEvenEl = (arr, delta) =>
-  Array.isArray(arr) ? arr.map(num => (num % 2 === 0 ? num + delta : num)) : null;
+const increaseEvenEl = (arr, delta) => {
+  if (!Array.isArray(arr)) return null;
+
+  return arr.map(num => (num % 2 === 0 ? num + delta : num));
+};
