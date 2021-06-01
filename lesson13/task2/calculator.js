8774db90577018ab2572c591f2c90d2eacaf93cd
Alex <mendelberg44@gmail.com>
2021-06-01 15:18:44 +0300

    l13 t2

--- lesson13/task2/calculator.js
+++ lesson13/task2/calculator.js
@@ -2,6 +2,6 @@ export const getSquaredArray =
   numbers => numbers.map(num => num ** 2);
 
 export const getOddNumbers = 
-  numbers => numbers.filter(num => num % 2 !== 0);
+  numbers => numbers.filter(num => num % 2 === 1);
 
 export default (a, b) => a + b;
