0b475dc440a8f773773b235354b9e98881ee946f
Alex <mendelberg44@gmail.com>
2021-06-02 16:35:03 +0300

    l24 a2

--- lesson24/add1/index.js
+++ lesson24/add1/index.js
@@ -1,49 +1,46 @@
-/* eslint-disable radix */
-const studentBirthDays = students => {
+/* eslint-disable guard-for-in */
+/* eslint-disable no-restricted-syntax */
+const studentsBirthDays = students => {
+  const obj = {};
   const months = [
-    { Jan: [] },
-    { Feb: [] },
-    { Mar: [] },
-    { Apr: [] },
-    { May: [] },
-    { Jun: [] },
-    { Jul: [] },
-    { Aug: [] },
-    { Sep: [] },
-    { Oct: [] },
-    { Nov: [] },
-    { Dec: [] },
+    'Jan',
+    'Feb',
+    'Mar',
+    'Apr',
+    'May',
+    'Jun',
+    'Jul',
+    'Aug',
+    'Sep',
+    'Oct',
+    'Nov',
+    'Dec',
   ];
 
-  const objNames = {};
-
-  students.forEach(el => {
-    const index = parseInt(el.birthDate) - 1;
-    const key = Object.keys(months[index]);
-    months[index][key].push(el);
+  students.forEach(elObj => {
+    const month = months[elObj.birthDate.split('/')[0] - 1];
+    obj[month] = obj[month] || [];
+    obj[month].push(elObj);
   });
 
-  months.forEach(month => {
-    const key = Object.keys(month);
-    if (month[key].length) {
-      month[key].sort((a, b) => a.birthDate.split('/')[1] - b.birthDate.split('/')[1]);
-      objNames[key] = month[key].map(obj => obj.name);
-    }
-  });
+  for (const key in obj) {
+    obj[key].sort((a, b) => a.birthDate.split('/')[1] - b.birthDate.split('/')[1]);
+  }
 
-  return objNames;
+  for (const key in obj) {
+    obj[key] = obj[key].map(student => student.name);
+  }
+
+  return obj;
 };
 
 const arr = [
-  { name: 'Tom', birthDate: '01/15/2010' },
   { name: 'Ben', birthDate: '01/17/2008' },
   { name: 'Sam', birthDate: '03/15/2010' },
+  { name: 'Tom', birthDate: '01/15/2010' },
   { name: 'Bob', birthDate: '03/08/2010' },
-  { name: 'John', birthDate: '03/31/2010' },
   { name: 'Tim', birthDate: '01/02/2010' },
+  { name: 'John', birthDate: '03/31/2010' },
 ];
 
-console.log(studentBirthDays(arr));
-
-      // month[key].forEach(ell => console.log(ell.birthDate.split('/')[1]));
-
+console.log(studentsBirthDays(arr));
