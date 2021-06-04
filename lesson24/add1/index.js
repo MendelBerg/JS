b6037755b7d46dff123d0fa633ed15646d1faf51
Alex <mendelberg44@gmail.com>
2021-06-04 15:48:27 +0300

    l24 add1

--- lesson24/add1/index.js
+++ lesson24/add1/index.js
@@ -1,37 +1,21 @@
-/* eslint-disable guard-for-in */
-/* eslint-disable no-restricted-syntax */
 const studentsBirthDays = students => {
-  const obj = {};
-  const months = [
-    'Jan',
-    'Feb',
-    'Mar',
-    'Apr',
-    'May',
-    'Jun',
-    'Jul',
-    'Aug',
-    'Sep',
-    'Oct',
-    'Nov',
-    'Dec',
-  ];
+  const birthObj = {};
 
   students.forEach(elObj => {
-    const month = months[elObj.birthDate.split('/')[0] - 1];
-    obj[month] = obj[month] || [];
-    obj[month].push(elObj);
+    const month = new Date(elObj.birthDate).toLocaleString('en', { month: 'short' });
+    birthObj[month] = birthObj[month] || [];
+    birthObj[month].push(elObj);
   });
 
-  for (const key in obj) {
-    obj[key].sort((a, b) => a.birthDate.split('/')[1] - b.birthDate.split('/')[1]);
-  }
-
-  for (const key in obj) {
-    obj[key] = obj[key].map(student => student.name);
-  }
-
-  return obj;
+  return Object.entries(birthObj).reduce(
+    (acc, month) => ({
+      ...acc,
+      [month[0]]: month[1]
+        .sort((a, b) => a.birthDate.split('/')[1] - b.birthDate.split('/')[1])
+        .map(({ name }) => name),
+    }),
+    {},
+  );
 };
 
 const arr = [
@@ -39,7 +23,7 @@ const arr = [
   { name: 'Sam', birthDate: '03/15/2010' },
   { name: 'Tom', birthDate: '01/15/2010' },
   { name: 'Bob', birthDate: '03/08/2010' },
-  { name: 'Tim', birthDate: '01/02/2010' },
+  { name: 'Tim', birthDate: '01/01/2010' },
   { name: 'John', birthDate: '03/31/2010' },
 ];
 
