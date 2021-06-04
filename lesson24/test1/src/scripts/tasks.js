6f265565925684517a5fab3c4f2bf0182f43b1bc
Alex <mendelberg44@gmail.com>
2021-06-04 17:07:16 +0300

    l24 test1

---
+++ lesson24/test1/src/scripts/tasks.js
@@ -0,0 +1,7 @@
+export const tasks = [
+  { text: 'Buy milk', done: false, date: Date.now() },
+  { text: 'Pick up Tom from airport', done: false, date: Date.now() },
+  { text: 'Visit party', done: false, date: Date.now() },
+  { text: 'Visit doctor', done: true, date: Date.now() },
+  { text: 'Buy meat', done: true, date: Date.now() },
+];
