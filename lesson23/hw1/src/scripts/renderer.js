2d0dd842e55175f7b3f44c763b5ca7de773d1fb6
Alex <mendelberg44@gmail.com>
2021-06-04 15:07:34 +0300

    l23 hw1

--- lesson23/hw1/src/scripts/renderer.js
+++ lesson23/hw1/src/scripts/renderer.js
@@ -1,6 +1,5 @@
-/* eslint-disable no-use-before-define */
-/* eslint-disable consistent-return */
 import { tasks } from './tasks.js';
+import { toggleTask } from './index.js';
 
 export const createCheckbox = done => {
   const checkboxElem = document.createElement('input');
@@ -34,25 +33,4 @@ export const renderTasks = tasksList => {
     .map(({ text, done }, index) => createLiElem(done, createCheckbox(done), text, index + 1));
 
   listElem.append(...liElems);
-};
-
-export const toggleTask = checkbox => {
-  const liElem = checkbox.parentElement;
-  liElem.classList.toggle('list__item_done');
-
-  const index = liElem.dataset.id - 1;
-  tasks[index].done = !tasks[index].done;
-
-  renderTasks(tasks);
-};
-
-export const createTask = () => {
-  const inputElem = document.querySelector('.task-input');
-
-  if (!inputElem.value) return null;
-
-  tasks.push({ text: inputElem.value, done: false });
-  inputElem.value = '';
-
-  renderTasks(tasks);
 };
