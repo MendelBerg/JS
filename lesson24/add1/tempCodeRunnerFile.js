d16df4be590a5e52c2831f254b423915e32d09cd
Alex <mendelberg44@gmail.com>
2021-06-05 21:19:03 +0300

    done

--- lesson24/add1/tempCodeRunnerFile.js
+++
@@ -1,5 +0,0 @@
-.forEach(elObj => {
-  //   const month = new Date(elObj.birthDate).toLocaleString('en', { month: 'short' });
-  //   birthObj[month] = birthObj[month] || [];
-  //   birthObj[month].push(elObj);
-  // });
--- lesson28/hw1/index.js
+++ lesson28/hw1/index.js
@@ -1,46 +1,26 @@
+/* eslint-disable no-eval */
 const shmoment = initialValue => {
   const result = new Date(initialValue);
 
-  const dateMethods = {
-    years: {
-      set: new Date().setFullYear,
-      get: new Date().getFullYear,
-    },
-    months: {
-      set: new Date().setMonth,
-      get: new Date().getMonth,
-    },
-    days: {
-      set: new Date().setDate,
-      get: new Date().getDate,
-    },
-    hours: {
-      set: new Date().setHours,
-      get: new Date().getHours,
-    },
-    minutes: {
-      set: new Date().setMinutes,
-      get: new Date().getMinutes,
-    },
-    seconds: {
-      set: new Date().setSeconds,
-      get: new Date().getSeconds,
-    },
-    milliseconds: {
-      set: new Date().setMilliseconds,
-      get: new Date().getMilliseconds,
-    },
+  const convertType = type => {
+    if (type === 'years') return 'FullYear';
+    if (type === 'months') return 'Month';
+    if (type === 'days') return 'Date';
+    return type[0].toUpperCase() + type.slice(1);
+  };
+
+  const count = (type, num, operator) => {
+    const currentTime = result[`get${type}`]();
+    result[`set${type}`](eval(`${currentTime} ${operator} ${num}`));
   };
 
   const calculator = {
     add(typeOfTime, num) {
-      const date = dateMethods[typeOfTime];
-      date.set.call(result, date.get.call(result) + num);
+      count(convertType(typeOfTime), num, '+');
       return this;
     },
     subtract(typeOfTime, num) {
-      const date = dateMethods[typeOfTime];
-      date.set.call(result, date.get.call(result) - num);
+      count(convertType(typeOfTime), num, '-');
       return this;
     },
     result() {
@@ -53,6 +33,10 @@ const shmoment = initialValue => {
 
 console.log(new Date(2020, 0, 7, 17, 17, 17));
 const dd = new Date(2020, 0, 7, 17, 17, 17);
-const res = shmoment(new Date(2020, 0, 7, 17, 17, 17)).add('minutes', 2).add('days', 8).subtract('years', 1).result(); // ... Jan 15 2019 17:19:17 ...
+const res = shmoment(new Date(2020, 0, 7, 17, 17, 17))
+  .add('seconds', 2)
+  .add('hours', 10)
+  .add('years', 10)
+  .result(); 
 console.log(res);
 console.log(dd);
